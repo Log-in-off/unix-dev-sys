8d6f3fc8eff26b0ba992b96f837ec1c7f356b8b1
Log-in-off <1386348@mail.ru>
2021-11-22 15:16:06 +0300

    Добавлена поддержка выбора файлов

--- myweb/myweb.c
+++ myweb/myweb.c
@@ -11,7 +11,8 @@
 #define HTTP_REQUEST_LEN 256
 #define HTTP_METHOD_LEN 6
 #define HTTP_URI_LEN 100
-#define HTTP_URL_PATH 100
+#define HTTP_URL_PATH 300
+#define FILE_NAME_LEN 500
 
 #define REQ_END 100
 #define ERR_NO_URI -100
@@ -28,6 +29,8 @@ struct http_req {
 	// server
 	// accept
 };
+
+char const *index_file = "/index.html";
 
 int fill_req(char *buf, struct http_req *req) {
 	if (strlen(buf) == 2) {
@@ -53,11 +56,12 @@ int fill_req(char *buf, struct http_req *req) {
         {
             //у нас пустой get запрос
             fprintf(stderr, "empty get  %s\n", find);
+            memcpy(req->url_path, index_file, strlen(index_file));
         }
         else
         {
-            fprintf(stderr, "clear path %s\n", req->url_path);
             find = strstr(path, "?");
+            //path++; // убрали '/'
             if (NULL == find)
             {
                 find = strstr(path, " HTTP");
@@ -69,6 +73,7 @@ int fill_req(char *buf, struct http_req *req) {
                 fprintf(stderr, "Don't have params\n");
                 memcpy(req->url_path, path, find-path);
                 req->url_path[find-path] = 0;
+                fprintf(stderr, "clear path %s\n", req->url_path);
             }
             else
             {
@@ -76,6 +81,7 @@ int fill_req(char *buf, struct http_req *req) {
                 memcpy(req->url_path, path, find-path);
                 req->url_path[find-path] = 0;
 
+                fprintf(stderr, "path %s\n", req->url_path);
                 find = strstr(path, " HTTP");
                 if (find == NULL)
                 {
@@ -86,8 +92,8 @@ int fill_req(char *buf, struct http_req *req) {
                 req->url_params[find-param] = 0;
             }
 
-            fprintf(stderr, "path %s\n", req->url_path);
-            fprintf(stderr, "params %s\n", req->url_params);
+            //fprintf(stderr, "path %s\n", req->url_path);
+            //fprintf(stderr, "params %s\n", req->url_params);
         }
 	}
 
@@ -114,17 +120,71 @@ int log_req(char *log_path, char *log_mes) {
 	return 0;
 }
 
-int make_resp(struct http_req *req) {
-    (void) req;
-	printf("HTTP/1.1 200 OK\r\n");
-	printf("Content-Type: text/html\r\n");
-	printf("\r\n");
-	printf("<html><body><title>Page title</title><h1>Page Header1</h1></body></html>\r\n");
-	return 0;
+int make_resp(char *base_path, struct http_req *req)
+{
+    int fdin;
+    struct stat statbuf;
+    void *mmf_ptr;
+    // определяем на основе запроса, что за файл открыть
+    char res_file[FILE_NAME_LEN] = "";
+
+    sprintf(res_file, "%s%s",base_path, req->url_path);
+    fprintf(stderr, "path file %s\n", res_file);
+    // открываем
+    if ((fdin=open(res_file, O_RDONLY)) < 0)
+    {
+        perror(res_file);
+        return 1;
+    }
+    // размер
+    if (fstat(fdin, &statbuf) < 0)
+    {
+        perror(res_file);
+        return 1;
+    }
+    // mmf
+    if ((mmf_ptr = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0)) == MAP_FAILED)
+    {
+        perror("myfile");
+        return 1;
+    }
+    // Выводим HTTP-заголовки
+    char *http_result = "HTTP/1.1 200 OK\r\n";
+    write(1,http_result,strlen(http_result));
+    char *http_contype = "Content-Type: text/html\r\n";
+    write(1,http_contype,strlen(http_contype));
+
+    char *header_end = "\r\n";
+    write(1,header_end,strlen(header_end));
+    // Выводим запрошенный ресурс
+    if (write(1,mmf_ptr,statbuf.st_size) != statbuf.st_size)
+    {
+        perror("stdout");
+        return 1;
+    }
+    // Подчищаем ресурсы
+    close(fdin);
+    munmap(mmf_ptr,statbuf.st_size);
+    return 0;
 }
+
+
+/*
+int make_resp(char *base_path, struct http_req *req)
+{
+    (void) req;
+    printf("HTTP/1.1 200 OK\r\n");
+    printf("Content-Type: text/html\r\n");
+    printf("\r\n");
+    printf("<html><body><title>Page title</title><h1>Page Header1</h1></body></html>\r\n");
+    return 0;
+}
+*/
+
 
 int main (void) {
 	char buf[HTTP_HEADER_LEN];
+
 	struct http_req req;
 	while(fgets(buf, sizeof(buf),stdin)) {
 		int ret = fill_req(buf, &req);
@@ -137,9 +197,8 @@ int main (void) {
 		else
 			// какая-то ошибка 
 			printf("Error: %d\n", ret);
-		
 	}
 
     log_req("some.log", "message");
-	make_resp(&req);
+    make_resp("/usr/local/bin/webpages", &req);
 }
